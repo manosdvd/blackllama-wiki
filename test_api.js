c868f767293fcddfc1ae2b3e53888632e0839241
David <manosdvd@gmail.com>
2026-07-10 14:39:20 -0700

    file was causing lint fail

--- test_api.js
+++
@@ -1,5 +0,0 @@
-const http = require('http');
-fetch('http://localhost:3000/api/alerts/fire')
-  .then(res => res.json())
-  .then(data => console.log(JSON.stringify(data, null, 2)))
-  .catch(console.error);
